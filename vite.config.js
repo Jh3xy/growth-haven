e098905d2d41d1517731aff7074441886eeb2011
Jh3xy <jhxydev.me@gmail.com>
2026-05-07 20:48:54 +0100

    Update landing to reflect project goals and features properly

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,27 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // We keep the root at '.' so it sees your main index.html
-  root: '.', 
+  root: ".",
   build: {
     rollupOptions: {
       input: {
-        main:      resolve(__dirname, 'index.html'),
-        admin:     resolve(__dirname, 'src/admin/index.html'),
-        login:     resolve(__dirname, 'src/login/index.html'),
-        terms: resolve(__dirname, 'src/terms/index.html'),
-        privacy: resolve(__dirname, 'src/privacy/index.html'),
-        recover:  resolve(__dirname, 'src/recover/index.html'),
-        register:  resolve(__dirname, 'src/register/index.html'),
-        dashboard: resolve(__dirname, 'src/dashboard/index.html'),
-        affiliate: resolve(__dirname, 'src/affiliate/index.html'),
-        casino:     resolve(__dirname, 'src/casino/index.html'),
-        mines:     resolve(__dirname, 'src/casino/mines/index.html'),
-        dice:     resolve(__dirname, 'src/casino/dice/index.html'),
-        limbo:     resolve(__dirname, 'src/casino/limbo/index.html'),
-        plinko:     resolve(__dirname, 'src/casino/plinko/index.html'),
-        'coin-flip':     resolve(__dirname, 'src/casino/coin-flip/index.html'),
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        admin: resolve(__dirname, "src/admin/index.html"),
+        login: resolve(__dirname, "src/login/index.html"),
+        terms: resolve(__dirname, "src/terms/index.html"),
+        privacy: resolve(__dirname, "src/privacy/index.html"),
+        recover: resolve(__dirname, "src/recover/index.html"),
+        register: resolve(__dirname, "src/register/index.html"),
+        dashboard: resolve(__dirname, "src/dashboard/index.html"),
+        affiliate: resolve(__dirname, "src/affiliate/index.html"),
+        post: resolve(__dirname, "src/post/index.html"),
+        casino: resolve(__dirname, "src/casino/index.html"),
+        mines: resolve(__dirname, "src/casino/mines/index.html"),
+        dice: resolve(__dirname, "src/casino/dice/index.html"),
+        limbo: resolve(__dirname, "src/casino/limbo/index.html"),
+        plinko: resolve(__dirname, "src/casino/plinko/index.html"),
+        "coin-flip": resolve(__dirname, "src/casino/coin-flip/index.html"),
+      },
+    },
+  },
+});
